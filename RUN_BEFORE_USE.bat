a1db1f7bd44f2dd0455206d45d0d42de8ba0e024
Lizhong <jerryzlz4@hotmail.com>
2020-04-27 22:33:41 +0800

    Add files via upload

--- RUN_BEFORE_USE.bat
+++ RUN_BEFORE_USE.bat
@@ -1,2 +1,9 @@
-copy .\geckodriver.exe %USERPROFILE%\AppData\Local\Temp
+@echo off
+if exist %windir%\SysWOW64 (
+ copy .\geckodriver.exe %USERPROFILE%\AppData\Local\Temp
+)else (
+ copy .\geckodriver32.exe %USERPROFILE%\AppData\Local\Temp
+)
 pause
+
+
