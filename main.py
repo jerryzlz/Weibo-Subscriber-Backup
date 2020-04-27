64ff938528bbf72483adcab34aa5baaf6b9b433d
Lizhong <jerryzlz4@hotmail.com>
2020-04-27 20:45:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Mail: jerryzlz4@hotmail.com
 
 class GetList(object):
 
-    def __init__(self, username, password):
+    def init(self, username, password):
         """
         初始化浏览器窗口
         :param username: 登录手机/邮箱
@@ -26,11 +26,11 @@ class GetList(object):
         self.driver.find_element_by_xpath('//*[@id="loginAction"]').click()
         time.sleep(3)
 
-    def get_follow_list(self):
+    def get_follow_list(self, speed=1.5):
         """
         获取关注列表
-        :param sleep_time:每次翻页的等候时间
-        :return:
+        :param speed: 等待翻页时间（单位：秒）
+        :return: None
         """
         print("=" * 100)
         print("开始获取关注列表")
@@ -55,7 +55,7 @@ class GetList(object):
                 self.driver.find_element_by_link_text("下页").click()
             except:
                 break
-            time.sleep(1)
+            time.sleep(speed)
         self.driver.find_element_by_xpath('/html/body/div[1]/a[1]').click()
         for i in range(len(follow_namelist)):
             follow_list.append([follow_namelist[i], follow_urllist[i]])
@@ -65,13 +65,14 @@ class GetList(object):
             output.write(",".join(f) + "\n")
         output.close()
         print("关注列表已成功保存")
+        print("文件名为：{}" .format(filename))
         print("=" * 100)
 
-    def get_fan_list(self):
+    def get_fan_list(self, speed=1.5):
         """
         获取粉丝列表
-        :param sleep_time: 每次翻页的等候时间
-        :return:
+        :param speed: 等待翻页时间（单位：秒）
+        :return: None
         """
         print("=" * 100)
         print("开始获取粉丝列表")
@@ -96,7 +97,7 @@ class GetList(object):
                 self.driver.find_element_by_link_text("下页").click()
             except:
                 break
-            time.sleep(1)
+            time.sleep(speed)
         self.driver.find_element_by_xpath('/html/body/div[1]/a[1]').click()
 
         for i in range(len(fan_namelist)):
@@ -107,14 +108,62 @@ class GetList(object):
             output.write(",".join(f) + "\n")
         output.close()
         print("粉丝列表已成功保存")
+        print("文件名为：{}" .format(filename))
         print("=" * 100)
 
-    def mutual_following(self):
+    def close(self):
+        self.driver.quit()
+
+    def difference(self, list1, list2):
         """
-        比对列表文件（暂未完成）
-        :return:
+        获得两个列表之间的差值
+        :param list1: 输入列表1
+        :param list2: 输入列表2
+        :return: 返回差值列表
         """
-        pass
+        list_dif = [i for i in list1 + list2 if i not in list1 or i not in list2]
+        return list_dif
+
+    def compare(self, old_filepath, new_filepath):
+        """
+        
+        :param old_filepath: 输入列表1的文件名
+        :param new_filepath: 输入列表2的文件名
+        :return: None
+        """
+        old_file, new_file = open(old_filepath, "r", encoding="utf-8"), open(new_filepath, "r", encoding="utf-8")
+        old_list, old_urllist, new_list, new_urllist, compared = [], [], [], [], []
+        for line in old_file:
+            old_list.append(line.strip("\n").split(","))
+        for line in new_file:
+            new_list.append(line.strip("\n").split(","))
+
+        for i in range(len(old_list)):
+            old_urllist.append(old_list[i][1])
+        for i in range(len(new_list)):
+            new_urllist.append(new_list[i][1])
+
+        for i in old_urllist:
+            if old_urllist.count(i) > 1:
+                old_urllist.remove(i)
+        for i in new_urllist:
+            if new_urllist.count(i) > 1:
+                new_urllist.remove(i)
+
+        compared = self.difference(old_urllist, new_urllist)
+        print(compared)
+        if len(compared) != 0:
+            filename = "compared_" + str(time.strftime("UTC%Y-%m-%d_%H-%M-%S", time.gmtime())) + ".txt"
+            output = open(str(filename), "w", encoding="utf-8")
+            for f in compared:
+                output.write(f + "\n")
+            output.close()
+            print("=" * 100)
+            print("粉丝列表已成功保存")
+            print("文件名为：{}".format(filename))
+        else:
+            print("=" * 100)
+            print("两个列表之间没有区别，未保存对比列表")
 
 
 while True:
@@ -122,29 +171,46 @@ while True:
     print("1.获取关注列表")
     print("2.获取粉丝列表")
     print("3.同时获取关注和粉丝列表")
-    print("4.使用两个本地已有的列表比对关注/粉丝变化情况（暂未完成）")
-    print("5.使用本地已有的列表比对当前关注/粉丝变化情况（暂未完成）")
-    print("6.退出本软件")
+    print("4.使用两个本地已有的列表比对列表之间变化情况")
+    print("0.退出本软件")
     print("=" * 100)
     num = int(input("请输入所需要的操作编号："))
     if num == 1:
+        print("=" * 100)
         name = input("请输入登录邮箱/手机号：")
         psd = input("请输入密码：")
-        main.get_follow_list()
+        main = GetList()
+        main.init(name, psd)
+        main.get_follow_list(0.5)
+        main.close()
     elif num == 2:
+        print("=" * 100)
         name = input("请输入登录邮箱/手机号：")
         psd = input("请输入密码：")
-        main = GetList(name, psd)
-        main.get_fan_list()
+        main = GetList()
+        main.init(name, psd)
+        main.get_fan_list(0.5)
+        main.close()
     elif num == 3:
+        print("=" * 100)
         name = input("请输入登录邮箱/手机号：")
         psd = input("请输入密码：")
-        main = GetList(name, psd)
-        main.get_follow_list()
-        main.get_fan_list()
-    elif num == 6:
+        main = GetList()
+        main.init(name, psd)
+        main.get_follow_list(0.5)
+        main.get_fan_list(0.5)
+        main.close()
+    elif num == 4:
+        print("=" * 100)
+        print("将输出两个列表的差值")
+        file1 = input("请输入第一个列表的文件名：")
+        file2 = input("请输入第二个列表的文件名：")
+        main = GetList()
+        main.compare(file1, file2)
+    elif num == 0:
         print("程序已关闭")
         break
     else:
+        print("=" * 100)
         print("输入错误，请重新输入")
         continue
